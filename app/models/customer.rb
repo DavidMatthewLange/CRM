45b413f01159ffd645241458f674f8de8fb67022
David Matthew Lange <david.m.lange@outlook.com>
2024-11-12 02:08:13 -0600

    Added customer partial

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,17 +2,13 @@ class Customer < ApplicationRecord
   has_one_attached :image
 
   validates :full_name, presence: true
-  validates :phone_numner, presence: true
+  validates :phone_number, presence: true
 
   def self.ransackable_associations(auth_object = nil)
     [ "image_attachment", "image_blob" ]
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "full_name", "phone_number", "email", "notes" ]
+    [ "full_name", "phone_number", "email", "notes", "image" ]
   end
-end
-
-def self.ransackable_attributes(auth_object = nil)
-  [ "blob_id", "created_at", "id", "name", "record_id", "record_type" ]
 end
